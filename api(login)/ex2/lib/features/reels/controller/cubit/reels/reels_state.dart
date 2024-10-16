a983a123563e5588f2d995c4b362ee8eda15b2d8
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2024-10-16 12:39:28 +0200

    add ex2 in api

---
+++ api(login)/ex2/lib/features/reels/controller/cubit/reels/reels_state.dart
@@ -0,0 +1,12 @@
+abstract class ReelsState {}
+
+class ReelsInitialState extends ReelsState {}
+
+class ReelsLoadingState extends ReelsState {}
+
+class ReelsSuccessState extends ReelsState {}
+
+class ReelsFailedState extends ReelsState {
+  final String msg;
+  ReelsFailedState({required this.msg});
+}
---
+++ api(login)/ex2/lib/features/reels/presentation/content_screen.dart
@@ -0,0 +1,62 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reels_video/features/reels/controller/cubit/content/content_cubit.dart';
+import 'package:reels_video/features/reels/controller/cubit/content/content_state.dart';
+import 'widgets/like_icon.dart';
+
+class ContentScreen extends StatelessWidget {
+  final String src;
+  const ContentScreen({super.key, required this.src});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ContentCubit(src),
+      child: BlocBuilder<ContentCubit, ContentState>(
+        builder: (context, state) {
+          final cubit = context.read<ContentCubit>();
+
+          if (state is ContentLoadingState) {
+            return const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(height: 10),
+                  Text("Loading....."),
+                ],
+              ),
+            );
+          }
+
+          if (state is ContentFailedState) {
+            return Center(
+              child: Text(state.msg),
+            );
+          }
+
+          if (state is ContentSuccessState && cubit.chewieController != null) {
+            return Stack(
+              fit: StackFit.expand,
+              children: [
+                GestureDetector(
+                  onDoubleTap: () {
+                    cubit.toggleLike();
+                  },
+                  child: Chewie(controller: cubit.chewieController!),
+                ),
+                if (state.isLiked)
+                  const Center(
+                    child: LikeIcon(),
+                  ),
+              ],
+            );
+          }
+
+          return const SizedBox.shrink();
+        },
+      ),
+    );
+  }
+}
